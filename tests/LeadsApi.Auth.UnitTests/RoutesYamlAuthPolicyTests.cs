e02dacec6b49c3e9ada42ff8b1a1951f66bf3745
ryandremza <ryan.dremza@gmail.com>
2026-02-21 20:23:09 +0000

    Load routes.yaml once per auth unit test fixture

--- tests/LeadsApi.Auth.UnitTests/RoutesYamlAuthPolicyTests.cs
+++ tests/LeadsApi.Auth.UnitTests/RoutesYamlAuthPolicyTests.cs
@@ -5,11 +5,18 @@ namespace LeadsApi.Auth.UnitTests;
 [TestFixture]
 public sealed class RoutesYamlAuthPolicyTests
 {
+    private ApiRoutesPolicy _routes = null!;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _routes = RoutesYamlPolicyLoader.Load();
+    }
+
     [Test]
     public void CreateLeadRoute_RequiresAuthenticatedUserAndLeadsImportScope()
     {
-        var routes = RoutesYamlPolicyLoader.Load();
-        var apiRoute = routes.GetRoute("POST", "/leads");
+        var apiRoute = _routes.GetRoute("POST", "/leads");
 
         Assert.That(apiRoute.Plugins.Auth.RequiresAuthenticationUser, Is.True);
         Assert.That(apiRoute.Plugins.Auth.RequiredScopes, Does.Contain("leads:import"));
@@ -19,8 +26,7 @@ public sealed class RoutesYamlAuthPolicyTests
     [Test]
     public void GetLeadRoute_RequiresAdviserOrCustomerRole()
     {
-        var routes = RoutesYamlPolicyLoader.Load();
-        var apiRoute = routes.GetRoute("GET", "/leads/{leadId}");
+        var apiRoute = _routes.GetRoute("GET", "/leads/{leadId}");
 
         Assert.That(apiRoute.Plugins.Auth.RequiresAuthenticationUser, Is.True);
         Assert.That(apiRoute.Plugins.Auth.RequiredRoles, Is.EquivalentTo(new[] { "adviser", "customer" }));
@@ -29,8 +35,7 @@ public sealed class RoutesYamlAuthPolicyTests
     [Test]
     public void AssignLeadRoute_RequiresAdviserAndManagerStaffTypeInOpa()
     {
-        var routes = RoutesYamlPolicyLoader.Load();
-        var apiRoute = routes.GetRoute("POST", "/leads/{leadId}/assign");
+        var apiRoute = _routes.GetRoute("POST", "/leads/{leadId}/assign");
 
         Assert.That(apiRoute.Plugins.Auth.RequiresAuthenticationUser, Is.True);
         Assert.That(apiRoute.Plugins.Auth.RequiredRoles, Is.EquivalentTo(new[] { "adviser" }));
